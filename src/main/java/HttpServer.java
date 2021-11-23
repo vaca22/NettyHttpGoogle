129465cbc4fe0e2181465ea7ac36efcdd5b599f5
liguanghao <liguanghao@viatomtech.com>
2021-11-23 11:44:07 +0800

    gfh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,8 +1,13 @@
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
@@ -21,17 +26,34 @@ public class HttpServer {
     }
 
     public void start() throws Exception{
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup work = new NioEventLoopGroup();
-        bootstrap.group(boss,work)
-                .handler(new LoggingHandler(LogLevel.DEBUG))
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new HttpServerInitializer());
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new HttpRequestDecoder());
+                            p.addLast(new HttpResponseEncoder());
+                            p.addLast(new CustomHttpServerHandler());
+                        }
+                    });
 
-        ChannelFuture f = bootstrap.bind(new InetSocketAddress(port)).sync();
+            ChannelFuture f = b.bind(port)
+                    .sync();
+            f.channel()
+                    .closeFuture()
+                    .sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
         System.out.println(" server start up on port : " + port);
-        f.channel().closeFuture().sync();
 
     }
 
